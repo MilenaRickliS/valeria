1cc163d033162e95aae20f1fe19c9c0d63b3813e
MilenaRickliS <engs-milenasilverio@camporeal.edu.br>
2024-06-22 12:21:50 -0300

    carousel

--- src/pages/Avaliacao/index.js
+++ src/pages/Avaliacao/index.js
@@ -2,7 +2,8 @@ import React from "react";
 import "./style.css";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
-
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
 import { useState, useEffect } from 'react';
 import { db } from '../../Firebase/firebaseConnection';
 import {
@@ -75,32 +76,32 @@ function Avaliacao() {
         
         <div>
           <h1>Avaliação</h1>
-          <div> 
-            <label>Data</label>
-            <input disabled
+          <div className="formulario"> 
+            
+            <input className="data-formulario" disabled
             placeholder={data}
             value={data}
             onChange={ (e) => setData(e.target.value) }
             /> <br/>
             <label>Qual o seu nome?</label>
-            <input 
+            <input className="input-formulario"
             placeholder='Digite o nome completo'
             value={name}
             onChange={ (e) => setName(e.target.value) }
             /> <br/>
             <label>Escreva um comentário:</label>
-            <textarea 
+            <textarea className="input-formulario"
             placeholder='Digite seu comentário'
             value={comentario}
             onChange={ (e) => setComentario(e.target.value) }
             /> <br/>
             <div>
-            <button className="button-cadastro" onClick={handleAdd}>Adicionar</button> 
+            <button className="button-formulario" onClick={handleAdd}>Adicionar</button> 
             </div>
 
           </div>
 
-         <ul className="prod">
+         {/* <ul className="prod">
             {avaliacao.map( (post) => {
             return(
             <li key={post.id} className="produtos">                
@@ -112,13 +113,24 @@ function Avaliacao() {
 
             ) 
             })}
-        </ul> 
+        </ul>  */}
+
+        
     
        
 
       </div>
-        
-        
+<div className="avaliacao">
+      <Carousel infiniteLoop={true} showStatus={false} showIndicators={false} emulateTouch={true} className="container-avaliacao">
+          {avaliacao.map((post) => (
+            <div key={post.id} className="div-avaliacao">
+              <strong className="nome-pessoa">{post.name}</strong>
+              <p className="data">{post.data}</p>
+              <p className="comentario">{post.comentario}</p>
+            </div>
+          ))}
+        </Carousel>
+        </div>    
       <Footer/>
       </div>
 
